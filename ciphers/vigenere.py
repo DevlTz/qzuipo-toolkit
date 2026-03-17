76fa74765007d121c9faff612f4de555e7d8ef21
DevlTz <kauazin3339@gmail.com>
2026-03-17 17:39:33 -0300

    feat(comments): comments add on the vigenere.py // Fixed analyzer.py

---
+++ ciphers/vigenere.py
@@ -0,0 +1,14 @@
+# 
+#Onde C é a mensagem criptografada, P texto plano, K a chave. E o i representa o índice.
+
+#Exemplo:
+
+#C=14+11 (Mod 26) => Portanto C=25
+
+
+#P=25-11+26(mod 26) => Portanto P=14
+
+
+#Chave de Criptografia
+# Mensagem em Texto Plano
+# Mensagem Criptografada
